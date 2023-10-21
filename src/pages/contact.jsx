01423e3b8221046be7059964f23954b02a0d8c6d
143Shailesh <93310989+143Shailesh@users.noreply.github.com>
2023-10-21 12:14:18 +0530

    Add files via upload

---
+++ src/pages/contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Contact = () => {
+  return <div>contact me <br />
+  <br />
+    call us:81152631283 ,
+    <br />
+    <br />
+    Email: sp983894@gmail.com
+</div>
+};
